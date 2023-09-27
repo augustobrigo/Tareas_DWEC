c94e8ef61633ca3bf068513e974268f97f7b34c0
augustobrigo <profeaugustobriga@gmail.com>
2023-09-27 16:07:52 +0200

    Update script.js

--- Practica7/js/script.js
+++ Practica7/js/script.js
@@ -1,114 +1,129 @@
-//Llamamos la body del documento
-const cuerpo = document.body;
+window.addEventListener("load", inicio);
 
-//Declaramos el contenedor principal que es un div
-let contenedorPrincipal = document.createElement("div");
-contenedorPrincipal.className = "container";
 
-//Creamos el segundo contenedor que va a tener la clase gallery
-let contenedor = document.createElement("fieldset");
+function inicio() {
+    var imagenReferencia;
+    let contenedorP = document.createElement("div");
+    contenedorP.className = "container";
+    let cuerpo = document.querySelector("body");
+    cuerpo.appendChild(contenedorP);
+    let contenedorR = document.createElement("div");
+    contenedorR.className = "gallery";
+    contenedorP.appendChild(contenedorR);
+    let contenedorS = document.createElement("fieldset");
+    contenedorS.className = "gallery";
+    let texto = document.createElement("legend");
 
-//Dos maneras de poner una clase a un elemento
-    //contenedor.setAttribute("class", "gallery");
-contenedor.className = "gallery";
+    texto.textContent = "Práctica 8";
 
-//Creamos la legenda propia del fieldset
-let legenda = document.createElement("legend");
-legenda.textContent = "Practica 8";
+    contenedorS.appendChild(texto);
+    contenedorP.appendChild(contenedorS);
 
-//Añadimos la legenda al fieldset
-contenedor.appendChild(legenda);
 
-let numBordes = 0;
+    refrescar();
 
-//Hacemos un bucle para insertar las imágenes aleatorias
-for(let i = 0; i < 20; i++){
+    var segundos = 30;
+    var temporizador = setInterval(refrescar, 10000);
 
-    let numRandom = Math.floor(Math.random() *20) + 7;
+    function refrescar() {
+        contenedorS.innerHTML = "";
+        contenedorR.innerHTML = "";
+        /**  let arrayImg = document.querySelectorAll("img");
+         console.log(arrayImg);
+         arrayImg.forEach(function (item, indice) {
+             contenedorS.removeChild(item);
+         }
+         )*/
+        ;
+        var contadorBordes = 0;
 
-    //Creamos el elemento img
-    let imagen = document.createElement("img");
-    //Le ponemos al elemento img sus atributos src
-    imagen.setAttribute("src", "https://randomuser.me/api/portraits/men/" + numRandom + ".jpg");
+        cargarImagenReferencia();
+        for (let i = 0; i < 20; i++) {
+            let num1 = Math.floor(Math.random() * 20);
+            let rutaH = "https://randomuser.me/api/portraits/men/";
+            let rutaM = "https://randomuser.me/api/portraits/women/";
 
-    //Añadimos inicialmente evento mouseover
-    imagen.onmouseover = function(){
-        imagen.style.transform = "scale(1.5)";
-    };
+            let imagen = document.createElement("img");
+            imagen.className = "gallery img";
+            contenedorS.appendChild(imagen);
+            if (i % 2 == 0) {
+                imagen.src = rutaH + num1 + ".jpg";
 
-    //Añadimos inicialmente evento mouseleave
-    imagen.onmouseleave = function(){
-        imagen.style.transform = "scale(1)";
-    };
+            } else {
+                imagen.src = rutaM + num1 + ".jpg";
+            }
 
-    //Evento de click
-    imagen.onclick = function(){
-        
-        if(imagen.className == "ponerBorde"){
+            //imagen.onmouseover = expandir;
+            //imagen.onmouseleave = encoger;
+            // imagen.onclick = borde;
+            imagen.onclick = comparar;
 
-            numBordes--;
-            imagen.className = "quitarBorde";
-            imagen.style.transform = "scale(1)";
-            imagen.onmouseleave = function(){
-                imagen.style.transform = "scale(1)";
-            };
+            function comparar() {
+                console.log(imagen.getAttribute("src"));
+                console.log("imagen de Referencia: " + imagenReferencia);
+                let imagenSelec = imagen.getAttribute("src");
+                let imagenRef = imagenReferencia;
 
-            imagen.onmouseover = function(){
+                if (imagenSelec == imagenRef) {
+                    alert("Acertaste");
+                } else {
+                    alert("Vaya paco");
+                }
+            }
+            function expandir() {
                 imagen.style.transform = "scale(1.5)";
-            };
+            }
 
-        }else if(imagen.className != "ponerBorde" && numBordes < 6){
-            
-            numBordes++;
-            imagen.className = "ponerBorde";
-            //imagen.style.transform = "scale(1.5)";
-            imagen.onmouseleave = function(){};
-            imagen.onmouseover = function(){};
+            function encoger() {
+                imagen.style.transform = "scale(1)";
+            }
 
-        }
+            function borde() {
+                if (imagen.className == "ponerBorde") {
+                    imagen.className = "quitarBorde";
+                    contadorBordes--;
+                    console.log(contadorBordes);
 
-        if(numBordes == 6){
-
-            let imagenesNuevas = document.querySelectorAll("img");
-
-            imagenesNuevas.forEach(function(item, posicion){
-
-                if(item.className != "ponerBorde"){
-                    item.onmouseover = function(){};
-
-                    item.style.opacity = "0.5";
+                } else {
+                    if (contadorBordes == 6) {
+                        return;
+                    }
+                    imagen.className = "ponerBorde";
+                    contadorBordes++;
+                    console.log(contadorBordes);
 
                 }
 
-            });
-
-        }else{
-
-            let imagenesNuevas = document.querySelectorAll("img");
-
-            imagenesNuevas.forEach(function(item, posicion){
-
-                if(item.className != "ponerBorde"){
-                    item.onmouseover = function(){
-                        item.style.transform = "scale(1.5)";
-                    };
-                    item.style.opacity = "";
+                if (contadorBordes == 6) {
+                    console.log("Desactivación de Bordes Operativa");
+                    let arrayImg = document.querySelectorAll("img");
+                    console.log(arrayImg);
+                    arrayImg.forEach(function (item, indice) {
+                        item.disabled = "true";
+                    }
+                    )
+                        ;
                 }
-
-            });
-
+            }
         }
-        
-        
-    };
+    }
 
-    //Añadimos la imagen ya creada al contenedor
-    contenedor.appendChild(imagen);
+    function cargarImagenReferencia() {
+        let rutaH = "https://randomuser.me/api/portraits/men/";
+        let rutaM = "https://randomuser.me/api/portraits/women/";
+        let num3 = Math.floor(Math.random() * 20);
+        let imagenR = document.createElement("img");
+        imagenR.className = "gallery img";
+        let num2 = Math.round(Math.random() * 2);
+        if (num2 == 1) {
+            imagenR.src = rutaM + num3 + ".jpg";
+        } else {
+            imagenR.src = rutaH + num3 + ".jpg";
+        }
+
+        contenedorR.appendChild(imagenR);
+        imagenReferencia = imagenR.src;
+    }
 
 }
-
-
-
-contenedorPrincipal.appendChild(contenedor);
-cuerpo.appendChild(contenedorPrincipal);
 
